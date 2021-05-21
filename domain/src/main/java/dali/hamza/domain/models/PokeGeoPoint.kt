43e06abb303c0c3b76a4966034c213f39653391d
dali <lionelviperdali@gmail.com>
2021-05-21 15:36:28 +0100

    * add PokeGeoPoint * create IPokemonRepository

---
+++ domain/src/main/java/dali/hamza/domain/models/PokeGeoPoint.kt
@@ -0,0 +1,6 @@
+package dali.hamza.domain.models
+
+data class PokeGeoPoint(
+    val lon:Double,
+    val lat:Double
+)
---
+++ domain/src/main/java/dali/hamza/domain/repository/IPokemonRepository.kt
@@ -0,0 +1,18 @@
+package dali.hamza.domain.repository
+
+import dali.hamza.domain.models.IResponse
+import dali.hamza.domain.models.PokeGeoPoint
+import dali.hamza.domain.models.Pokemon
+import kotlinx.coroutines.flow.Flow
+
+interface IPokemonRepository : IBaseRepository<Pokemon> {
+
+    suspend fun getRandomPokemonLocationFlow(
+        currentLocation: PokeGeoPoint,
+    ): Flow<IResponse>
+
+    suspend fun getAllFlow(filter: String): Flow<IResponse>
+
+    suspend fun getCommunityPokemonFlow(): Flow<IResponse>
+
+}
