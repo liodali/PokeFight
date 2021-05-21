0f589ccc54a829e44130c9e8f748e25d338c597d
dali <lionelviperdali@gmail.com>
2021-05-21 19:10:47 +0100

    * preparation di appModule

---
+++ app/src/main/java/dali/hamza/pokemongofight/Application.kt
@@ -0,0 +1,7 @@
+package dali.hamza.pokemongofight
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PokemonApplication:Application()
---
+++ app/src/main/java/dali/hamza/pokemongofight/di/AppModule.kt
@@ -0,0 +1,41 @@
+package dali.hamza.pokemongofight.di
+
+import android.app.Application
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import dali.hamza.pokemongofight.R
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    fun provideBaseUrl(application: Application) = application.getString(R.string.serverUrl)
+
+    @Provides
+    fun provideMoshi(): MoshiConverterFactory = MoshiConverterFactory.create()
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient = OkHttpClient.Builder()
+        .build()
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        BASE_URL: String,
+        moshiConverter: MoshiConverterFactory
+    ): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(moshiConverter)
+            .build()
+}
