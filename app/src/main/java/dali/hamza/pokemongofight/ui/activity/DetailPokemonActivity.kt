28a5cffe818da90c98816364a2d4214fd4bb5d2d
dali <lionelviperdali@gmail.com>
2021-05-26 00:59:55 +0100

    #10 * create collapse toolbar * fix bug

--- app/src/main/java/dali/hamza/pokemongofight/ui/activity/DetailPokemonActivity.kt
+++ app/src/main/java/dali/hamza/pokemongofight/ui/activity/DetailPokemonActivity.kt
@@ -5,15 +5,31 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.os.bundleOf
+import com.google.android.material.appbar.MaterialToolbar
+import com.squareup.picasso.Picasso
+import dali.hamza.domain.models.PokeGeoPoint
 import dali.hamza.domain.models.Pokemon
+import dali.hamza.domain.models.PokemonWithGeoPoint
 import dali.hamza.domain.models.UserPokemon
 import dali.hamza.pokemongofight.R
+import dali.hamza.pokemongofight.databinding.ActivityDetailPokemonBinding
 
 class DetailPokemonActivity : AppCompatActivity() {
 
+
+    private lateinit var binding: ActivityDetailPokemonBinding
+    private lateinit var toolbar: MaterialToolbar
+
+
+    private var pokemonGeoPoint: PokemonWithGeoPoint? = null
     private var pokemon: Pokemon? = null
+    private var pokeGeoPoint: PokeGeoPoint? = null
     private var pokemonUser: UserPokemon? = null
     private var typeDetail: String? = null
+
+    private var isCaptured: Boolean = false
+    private var isWild: Boolean = false
+    private var isOther: Boolean = false
 
 
     companion object {
@@ -43,7 +59,63 @@ class DetailPokemonActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_pokemon)
+        binding = ActivityDetailPokemonBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        retrieveDataIntent()
+        toolbar = binding.idToolbarDetailPokemon
+        setSupportActionBar(toolbar)
+        title = pokemon?.name ?: ""
+
+        Picasso.get()
+            .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon!!.id}.png")
+            .into(binding.idImgFrontPokemon)
+
+        Picasso.get()
+            .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${pokemon!!.id}.png")
+            .into(binding.idImgBackPokemon)
+
+    }
+
+    private fun retrieveDataIntent() {
+        when {
+            intent.extras!!.containsKey(keyPoke) -> {
+                pokemonGeoPoint = intent.extras!!.get(keyPoke) as PokemonWithGeoPoint?
+                pokemon = pokemonGeoPoint!!.pokemon
+                pokeGeoPoint = pokemonGeoPoint!!.pokeGeoPoint
+            }
+            intent.extras!!.containsKey(keyPokeUser) -> {
+                pokemonUser = intent.extras!!.get(keyPokeUser) as UserPokemon?
+                pokemon = pokemonUser!!.pokemon
+
+            }
+            intent.extras!!.containsKey(keyMePoke) -> {
+                pokemonGeoPoint = intent.extras!!.get(keyPokeUser) as PokemonWithGeoPoint?
+                pokemon = pokemonGeoPoint!!.pokemon
+                pokeGeoPoint = pokemonGeoPoint!!.pokeGeoPoint
+            }
+            intent.extras!!.containsKey(keyTypeDetail) -> {
+                when (intent.extras!!.getString(keyTypeDetail)!!.lowercase()) {
+                    "me" -> {
+                        isCaptured = true
+                        isWild = false
+                        isOther = false
+                    }
+                    "wild" -> {
+                        isCaptured = false
+                        isWild = true
+                        isOther = false
+                    }
+                    "friends", "foes" -> {
+                        isCaptured = false
+                        isWild = false
+                        isOther = true
+                    }
+                    else -> {
+                        finish()
+                    }
+                }
+            }
+        }
     }
 
     override fun onBackPressed() {
