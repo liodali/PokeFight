c2d12ba87f417be9c1b5b265bf265f431de3645b
dali <lionelviperdali@gmail.com>
2021-05-25 20:50:19 +0100

    #9 * show data from api when no captured pokemon exist in local database * fix bug

---
+++ app/src/main/java/dali/hamza/pokemongofight/ui/adapter/MyTeamListAdapter.kt
@@ -0,0 +1,59 @@
+package dali.hamza.pokemongofight.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.viewbinding.ViewBinding
+import dali.hamza.core.utilities.DateManager
+import dali.hamza.domain.models.Community
+import dali.hamza.domain.models.Pokemon
+import dali.hamza.domain.models.PokemonWithGeoPoint
+import dali.hamza.domain.models.UserPokemon
+import dali.hamza.pokemongofight.R
+import dali.hamza.pokemongofight.databinding.ItemListCommunityPokemonBinding
+import dali.hamza.pokemongofight.databinding.ItemPokmenMyTeamBinding
+
+class MyTeamListAdapter constructor(
+    val action: MyTeamItemCallback
+) : BaseAdapter<PokemonWithGeoPoint, MyTeamListAdapter.ItemViewHolder>() {
+
+    /// viewHolder
+    class ItemViewHolder(
+        private val bindingView: ItemPokmenMyTeamBinding,
+        val action: MyTeamItemCallback
+    ) : BaseAdapter.BaseViewHolder<PokemonWithGeoPoint>(bindingView) {
+        override fun bind(data: PokemonWithGeoPoint) {
+                bindingView.idNamePokeMyTeam.text = data.pokemon.name
+                bindingView.idTimeCapturedMyTeam.text = bindingView.root.context.resources.getString(
+                    R.string.captured_at) + data.pokemon.captured_at
+        }
+
+        companion object {
+            fun create(
+                parent: ViewGroup,
+                action: MyTeamItemCallback
+            ): ItemViewHolder {
+                val inflater = LayoutInflater.from(parent.context)
+                val binding = ItemPokmenMyTeamBinding.inflate(inflater, parent, false)
+                return ItemViewHolder(
+                    binding,
+                    action = action
+                )
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        return ItemViewHolder.create(
+            parent = parent,
+            action = action
+        )
+    }
+
+    interface MyTeamItemCallback {
+        fun goToDetailPokemon(
+            pokemon: UserPokemon,
+            type: String,
+        )
+    }
+}
--- app/src/main/java/dali/hamza/pokemongofight/ui/fragment/MyTeamFragment.kt
+++ app/src/main/java/dali/hamza/pokemongofight/ui/fragment/MyTeamFragment.kt
@@ -5,8 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import dagger.hilt.android.AndroidEntryPoint
+import dali.hamza.domain.models.*
 import dali.hamza.pokemongofight.R
+import dali.hamza.pokemongofight.common.gone
+import dali.hamza.pokemongofight.common.visible
+import dali.hamza.pokemongofight.databinding.EmptyBinding
+import dali.hamza.pokemongofight.databinding.FragmentMyTeamBinding
+import dali.hamza.pokemongofight.ui.adapter.MyTeamListAdapter
+import dali.hamza.pokemongofight.viewmodels.MyTeamViewModel
+import kotlinx.coroutines.flow.collect
 
 
 /**
@@ -15,8 +28,22 @@ import dali.hamza.pokemongofight.R
  * create an instance of this fragment.
  */
 @AndroidEntryPoint
-class MyTeamFragment : Fragment() {
+class MyTeamFragment : Fragment(), MyTeamListAdapter.MyTeamItemCallback {
 
+
+    private lateinit var binding: FragmentMyTeamBinding
+    private lateinit var swipeRefreshList: SwipeRefreshLayout
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var loadingView: View
+    private lateinit var emptyView: EmptyBinding
+
+
+    private val myTeamListAdapter: MyTeamListAdapter by lazy {
+        MyTeamListAdapter(this)
+    }
+
+
+    private val viewModel: MyTeamViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,9 +53,68 @@ class MyTeamFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_team, container, false)
+        binding = FragmentMyTeamBinding.inflate(inflater, container, false)
+        recyclerView = binding.idRecyclerMyTeamList
+        swipeRefreshList = binding.idSwipeRefreshListMyTeam
+        emptyView = binding.idEmptyListMyteam
+        loadingView = binding.idLoadingMyTeam.root
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recyclerView.adapter = myTeamListAdapter
+        recyclerView.layoutManager =
+            GridLayoutManager(
+                requireContext(),
+                1,
+                GridLayoutManager.VERTICAL,
+                false
+            )
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.getFlowCommunityPokemon().collect { response ->
+                if (response != null) {
+                    buildUI(response)
+                    swipeRefreshList.visible()
+                    loadingView.gone()
+                }
+            }
+        }
+        viewModel.fetchForCommunityPokemon()
+    }
+
+    private fun buildUI(response: IResponse) {
+        when (response) {
+            is MyResponse.SuccessResponse<*> -> {
+                val listMyTeam = response.data as List<PokemonWithGeoPoint>
+                myTeamUI(listMyTeam)
+            }
+            else -> {
+                showEmptyListMyTeam()
+            }
+        }
+    }
+
+    private fun myTeamUI(listMyTeam: List<PokemonWithGeoPoint>) {
+        when (listMyTeam.isEmpty()) {
+            false -> {
+                myTeamListAdapter.addListWithClear(listMyTeam)
+                recyclerView.visible()
+                emptyView.root.gone()
+            }
+            else -> {
+                showEmptyListMyTeam()
+            }
+        }
+    }
+
+    private fun showEmptyListMyTeam() {
+        recyclerView.gone()
+        emptyView.idEmptyList.visible()
     }
 
     companion object {
@@ -39,5 +125,9 @@ class MyTeamFragment : Fragment() {
             MyTeamFragment().apply {
                 arguments = Bundle()
             }
+    }
+
+    override fun goToDetailPokemon(pokemon: UserPokemon, type: String) {
+        TODO("Not yet implemented")
     }
 }
