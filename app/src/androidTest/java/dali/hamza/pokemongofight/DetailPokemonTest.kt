562cfb9cbfce040a1cf874f4c916582ce088fd78
dali <lionelviperdali@gmail.com>
2021-05-27 10:15:50 +0100

    #10 * fix test of DetailPokemonActivity

--- app/src/androidTest/java/dali/hamza/pokemongofight/DetailPokemonTest.kt
+++ app/src/androidTest/java/dali/hamza/pokemongofight/DetailPokemonTest.kt
@@ -1,16 +1,22 @@
 package dali.hamza.pokemongofight
 
 
+import android.app.Activity
 import android.app.Instrumentation
 import android.content.Intent
 import android.os.Bundle
+import android.view.View.GONE
 import androidx.core.app.ComponentActivity
 import androidx.core.os.bundleOf
+import androidx.core.view.isVisible
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
+import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
@@ -21,6 +27,7 @@ import dali.hamza.domain.models.PokeGeoPoint
 import dali.hamza.domain.models.Pokemon
 import dali.hamza.domain.models.PokemonWithGeoPoint
 import dali.hamza.pokemongofight.ui.activity.DetailPokemonActivity
+import org.junit.After
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -28,7 +35,13 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class DetailPokemonTest {
 
-    val intent =
+    private lateinit var scenario: ActivityScenario<DetailPokemonActivity>
+
+    private lateinit var intent: Intent
+
+
+    @get:Rule
+    var activityScenarioRule = ActivityScenarioRule<DetailPokemonActivity>(
         Intent(ApplicationProvider.getApplicationContext(), DetailPokemonActivity::class.java)
             .putExtras(
                 bundleOf(
@@ -47,17 +60,35 @@ class DetailPokemonTest {
                     DetailPokemonActivity.keyTypeDetail to "me"
                 )
             )
-
-    @get:Rule
-    var activityScenarioRule = ActivityScenarioRule<DetailPokemonActivity>(
-        intent
     )
 
+    @After
+    fun cleanup() {
+        scenario.close()
+    }
 
     @Test
-    fun testEvent() {
-
-        val scenario = activityScenarioRule.scenario
+    fun testMyTeam() {
+        intent =
+            Intent(ApplicationProvider.getApplicationContext(), DetailPokemonActivity::class.java)
+                .putExtras(
+                    bundleOf(
+                        DetailPokemonActivity.keyMePoke to PokemonWithGeoPoint(
+                            pokemon = Pokemon(
+                                id = 1,
+                                name = "balbosoura",
+                                url = "https://pokeapi.co/api/v2/pokemon/1/",
+                                captured_at = DateManager.dateFormat_full.format(DateManager.now())
+                            ),
+                            pokeGeoPoint = PokeGeoPoint(
+                                lat = 0.0,
+                                lon = 0.0
+                            )
+                        ),
+                        DetailPokemonActivity.keyTypeDetail to "me"
+                    )
+                )
+        scenario = activityScenarioRule.scenario
         scenario.moveToState(Lifecycle.State.CREATED)
         var title = ""
         scenario.onActivity {
@@ -67,6 +98,51 @@ class DetailPokemonTest {
 
         scenario.moveToState(Lifecycle.State.RESUMED)
         onView(withId(R.id.id_captured_pokemon_fl_bt)).check(matches(isDisplayed()))
+
+    }
+
+    @Test
+    fun testWild() {
+
+        intent = Intent(
+            ApplicationProvider.getApplicationContext(), DetailPokemonActivity::class.java
+        ).apply {
+            this.putExtras(
+                bundleOf(
+                    DetailPokemonActivity.keyPoke to PokemonWithGeoPoint(
+                        pokemon = Pokemon(
+                            id = 1,
+                            name = "balbosoura",
+                            url = "https://pokeapi.co/api/v2/pokemon/1/",
+                            captured_at = DateManager.dateFormat_full.format(DateManager.now())
+                        ),
+                        pokeGeoPoint = PokeGeoPoint(
+                            lat = 0.0,
+                            lon = 0.0
+                        )
+                    ),
+                    DetailPokemonActivity.keyTypeDetail to "wild"
+                )
+            )
+        }
+        scenario = ActivityScenario.launch(
+            intent
+        )
+
+        scenario.moveToState(Lifecycle.State.CREATED)
+        var title = ""
+        scenario.onActivity {
+            title = it.title.toString()
+        }
+        assert(title == "balbosoura")
+
+        scenario.moveToState(Lifecycle.State.RESUMED)
+        onView(withId(R.id.id_captured_pokemon_fl_bt)).check{ view, noViewFoundException ->
+            assert(!view.isVisible)
+        }
+        onView(withId(R.id.id_bt_capture_pokemon)).check { view, noViewFoundException ->
+            assert(view.isVisible)
+        }
 
     }
 }
